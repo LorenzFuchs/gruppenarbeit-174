cc2279c741326416362e39a5d2d901439361a4e8
DennisLudmann <dennisludmann@gmail.com>
2022-02-18 21:09:38 +0100

    addtasks backendupload plus NO auto reload

--- addtask.js
+++ addtask.js
@@ -2,12 +2,15 @@ let tasks = [];
 
 
 function createTask() {
+
+    event.preventDefault();                             // stops auto reload of the page onsubmit
     let title = document.getElementById('title');
     let category = document.getElementById('category');
     let description = document.getElementById('description');
     let date = document.getElementById('date');
     let agency = document.getElementById('agency');
 
+    
     let task = {
         "title": title.value,
         "category": category.value,
@@ -18,11 +21,18 @@ function createTask() {
 
     tasks.push(task);
     console.log(tasks);
+    backend.setItem('tasks', JSON.stringify(tasks));        //backend connection
+    clearForm();
+    
+    
 
+}
+
+function clearForm(){
     title.value = '';
     category.value = '';
     description.value = '';
     date.value = '';
     agency.value = '';
-
 }
+
--- backlog.js
+++ backlog.js
@@ -1,14 +1,27 @@
 let allTasks =[];
+let users = [];
+let activeTask =[];
 
+/*
 async function loadAllTasks(){
     await downloadFromServer();
     allTasks = JSON.parse(backend.getItem('tasks')) || [];
 
 }
+*/
 
+function addUser() {
+    users.push('Klaus');
+    backend.setItem('users', JSON.stringify(users));
+}
 
-
+async function init() {
+    await downloadFromServer();
+    users = JSON.parse(backend.getItem('tasks')) || [];
+}
  
+
+
 /*
 async function init() {
     await downloadFromServer();
@@ -19,4 +32,5 @@ function loadAllTasks(){
     let allTasksAsString = localStorage.getItem('allTasks');
     allTasks = JSON.parse(allTasksAsString);
 }
+{"tasks":"[{\"title\":\"MEINS\",\"category\":\"Testing\",\"description\":\"wr ar ar\",\"date\":\"2022-02-16\",\"agency\":\"High\"}]"}
 */
