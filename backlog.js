47ba9467fa3f23e93fe82c19d7f20f09c46659a2
DennisLudmann <dennisludmann@gmail.com>
2022-02-25 20:02:47 +0100

    icons and stat of functions

--- backlog.js
+++ backlog.js
@@ -1,6 +1,6 @@
 let allTasks = [];
 let activeTasks = [];
-let task =[];
+let task = [];
 
 async function init() {
     await downloadFromServer();
@@ -9,33 +9,39 @@ async function init() {
     console.log(allTasks);
     AddTicket();
 }
- 
+
 function AddTicket() {
-    document.getElementById('ticket-wrapper').innerHTML = ``; 
-   
+    document.getElementById('ticket-wrapper').innerHTML = ``;
+
     for (let i = 0; i < allTasks.length; i++) {
         task = allTasks[i];
-    
-    document.getElementById('ticket-wrapper').innerHTML += `
-    <div id="id${i}"onclick="activeTask('${i}')" class="ticket b-l-${task['category']}">
+        //onclick= onblur="activeTask('${i}')" onblur="myFunction() onblur="activeTask('${i}')"
+        document.getElementById('ticket-wrapper').innerHTML += `
+    <div id="id${i}" contentEditable="true"  class="ticket b-l-${task['category']}">
     <div class="left-container">
     <img src="${task['profil']}" class="ticket-img">
-    <div class="name-mail">
-        <p>Gus K. Medina</p>
-        <a href="#">gusmedina@gmail.com</a>
+    <div contentEditable="true"class="name-mail">
+        <p id="name${i}" contentEditable="true">Gus K. Medina</p>
+        <a id="mail${i}" contentEditable="true" href="#">gusmedina@gmail.com</a>
     </div>
 </div>
-    <div class="ticket-category">${task['category']}</div>
+    <div id="category${i}" contentEditable="true" class="ticket-category">${task['category']}</div>
     <div class="ticket-details">
-        <p>${task['description']} </p>
+        <p id="description${i}" contentEditable="true">${task['description']} </p>
     </div>
-    <img class="edit" src="img/pen.png" alt="pen">
+    <div class="column">
+    <img onclick="editable('${i}')" class="edit" src="img/pen.png" alt="pen">
+    <img onclick="activeTask('${i}')" class="edit" src="img/lunch.png">
+    <img onclick="saveTask('${i}')" class="edit" src="img/save.png">
+    </div>
 </div>`
-;
-}
+            ;
+    }
 }
 
-function activeTask(i){             //function to pass on JSON and remove from backlog
+
+
+function activeTask(i) {             //function to pass on JSON and remove from backlog
     let activeTask = {
         "title": allTasks[i]['title'],
         "category": allTasks[i]['category'],
@@ -49,24 +55,54 @@ function activeTask(i){             //function to pass on JSON and remove from b
     console.log(activeTasks);
     removeTask(i);
     backend.setItem('activeTasks', JSON.stringify(activeTasks));
-    
+
 }
 
 function removeTask(i) {
     feedbackSnackbar();
     var j = "id" + i;
-   var element = document.getElementById(j);
-   element.classList.add("d-none");
-   allTasks.splice(i, 1);
-   backend.setItem('tasks', JSON.stringify(allTasks)); 
+    var element = document.getElementById(j);
+    element.classList.add("d-none");
+    allTasks.splice(i, 1);
+    backend.setItem('tasks', JSON.stringify(allTasks));
 }
 
 
 
 
 function feedbackSnackbar() {       // w3 Snackbar / Toast
-  var toast = document.getElementById("snackbar");
-  toast.className = "show";
-  setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 2000);
+    var toast = document.getElementById("snackbar");
+    toast.className = "show";
+    setTimeout(function () { toast.className = toast.className.replace("show", ""); }, 2000);
 }
 
+
+function editable(i) {
+    debugger;
+    var j = "description" + i;
+    document.getElementById(j).focus();
+} 
+
+/*
+function saveTask(i){
+    allTasks[i]==
+    "title": allTasks[i]['title'],
+    "category": document.getcategory(i),
+    "description": allTasks[i]['description'],
+    "date": allTasks[i]['date'],
+    "urgency": allTasks[i]['urgency'],
+allTasks[i]
+}
+
+<p id="name${i}" contentEditable="true">Gus K. Medina</p>
+<a id="mail${i}" contentEditable="true" href="#">gusmedina@gmail.com</a>
+</div>
+</div>
+<div id="category${i}" contentEditable="true" class="ticket-category">${task['category']}</div>
+<div class="ticket-details">
+<p id="description${i}" contentEditable="true">${task['description']} </p>
+</div>
+<div class="column">
+<img onclick="editable('${i}')" class="edit" src="img/pen.png" alt="pen">
+<img onclick="activeTask('${i}')" class="edit" src="img/lunch.png">
+<img onclick="saveTask('${i}')" class="edit" src="img/save.png"> */
