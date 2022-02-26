d39fdbad99b674fc25c59a39418d4107a61f4363
DennisLudmann <dennisludmann@gmail.com>
2022-02-26 21:23:46 +0100

    editTask

--- backlog.js
+++ backlog.js
@@ -1,6 +1,6 @@
 let allTasks = [];
 let activeTasks = [];
-let task = [];
+
 
 async function init() {
     await downloadFromServer();
@@ -15,7 +15,7 @@ function AddTicket() {
 
     for (let i = 0; i < allTasks.length; i++) {
         task = allTasks[i];
-        //onclick= onblur="activeTask('${i}')" onblur="myFunction() onblur="activeTask('${i}')"
+        
         document.getElementById('ticket-wrapper').innerHTML += `
     <div id="id${i}" contentEditable="true"  class="ticket b-l-${task['category']}">
     <div class="left-container">
@@ -30,7 +30,7 @@ function AddTicket() {
         <p id="description${i}" contentEditable="true">${task['description']} </p>
     </div>
     <div class="column">
-    <img onclick="editable('${i}')" class="icons" src="img/pen.png" alt="pen">
+    <img onclick="editTask('${i}')" class="icons" src="img/pen.png" alt="pen">
     <img onclick="activeTask('${i}')" class="icons" src="img/lunch.png">
     <img onclick="saveTask('${i}')" class="icons" src="img/save.png">
     </div>
@@ -55,13 +55,12 @@ function activeTask(i) {             //function to pass on JSON and remove from 
     console.log(activeTasks);
     removeTask(i);
     backend.setItem('activeTasks', JSON.stringify(activeTasks));
-
 }
 
 function removeTask(i) {
     feedbackSnackbar();
-    var j = "id" + i;
-    var element = document.getElementById(j);
+    let j = "id" + i;
+    let element = document.getElementById(j);
     element.classList.add("d-none");
     allTasks.splice(i, 1);
     backend.setItem('tasks', JSON.stringify(allTasks));
@@ -77,17 +76,52 @@ function feedbackSnackbar() {       // w3 Snackbar / Toast
 }
 
 
-function editable(i) {
-    debugger;
-    var j = "description" + i;
-    document.getElementById(j).focus();
-} 
+function editTask(i) {
+    let title = allTasks[i]['title'];
+    let category = document.getElementById(`category${i}`).innerHTML;
+    let description = document.getElementById(`description${i}`).innerHTML;
+    let date = allTasks[i]['date'];
+    let urgency = allTasks[i]['urgency'];
+    let asignedto = document.getElementById(`name${i}`).innerHTML;
+    console.log(category, description, date, urgency, asignedto);
+
+    let task = {
+
+        'title': title,
+        'category': category,
+        'description': description,
+        'date': date,
+        'urgency': urgency,
+        'asignedto': asignedto
+
+    }
+    console.log(task);
+    allTasks.splice(i, 1, task);
+    backend.setItem('allTasks', JSON.stringify(allTasks));
+    AddTicket();
+
+}
 
 /*
+"title": allTasks[i]['title'],
+"category": allTasks[i]['category'],
+"description": allTasks[i]['description'],
+"date": allTasks[i]['date'],
+"urgency": allTasks[i]['urgency'],
+"profil": allTasks[i]['profil'],
+'status': 'toDo'
+
+let title = document.getElementById('title');
+    let category = document.getElementById('category');
+    let description = document.getElementById('description');
+    let date = document.getElementById('date');
+    let urgency = document.getElementById('urgency');
+    let asignedto = document.getElementById('asignedto');
+
 function saveTask(i){
     allTasks[i]==
     "title": allTasks[i]['title'],
-    "category": document.getcategory(i),
+    "category": document.getElementById('category(i)'),
     "description": allTasks[i]['description'],
     "date": allTasks[i]['date'],
     "urgency": allTasks[i]['urgency'],
@@ -105,4 +139,10 @@ allTasks[i]
 <div class="column">
 <img onclick="editable('${i}')" class="edit" src="img/pen.png" alt="pen">
 <img onclick="activeTask('${i}')" class="edit" src="img/lunch.png">
-<img onclick="saveTask('${i}')" class="edit" src="img/save.png"> */
+<img onclick="saveTask('${i}')" class="edit" src="img/save.png"> 
+
+
+// Get text content
+var text = elem.textContent;
+*/
+//onclick= onblur="activeTask('${i}')" onblur="myFunction() onblur="activeTask('${i}')"
